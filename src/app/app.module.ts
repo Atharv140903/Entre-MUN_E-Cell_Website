ab64d792fba8e434a7857d8517d3741b21e3ae75
Atharv140903 <atharvkurde2003@gmail.com>
2022-11-17 05:25:02 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AlertModule } from 'ngx-bootstrap/alert';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,9 @@ import { PreparationComponent } from './pages/preparation/preparation.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot(),
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,41 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None
+
 })
 export class HomeComponent implements OnInit {
+  
+ 
+  toggleMenu(): void {
+    var element = <HTMLInputElement>document.getElementById("myLinks")
+    element.classList.toggle("s3");
+  }
+
+  faqShow1(): void {
+    var element = <HTMLInputElement>document.getElementById("c4")
+    element.classList.toggle("s3");
+  }
+
+  faqShow2(): void {
+    var element = <HTMLInputElement>document.getElementById("c5")
+    element.classList.toggle("s3");
+  }
+
+  faqShow3(): void {
+    var element = <HTMLInputElement>document.getElementById("c6")
+    element.classList.toggle("s3");
+  }
+
+  faqShow4(): void {
+    var element = <HTMLInputElement>document.getElementById("c7")
+    element.classList.toggle("s3");
+  }
+
 
   constructor() { }
 
   ngOnInit(): void {
 
-    /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-    function myFunction() {
-      var x = (<HTMLInputElement>document.getElementById("myLinks"));
-      if (x.style.display === "block") {
-        x.style.display = "none";
-      } else {
-        x.style.display = "block";
-      }
-
-      return ;
-    }
-
-    let answers = document.querySelectorAll(".fa");
-    answers.forEach((event) => {
-      event.addEventListener("click", () => {
-        if (event.classList.contains("active")) {
-          event.classList.remove("active");
-        } else {
-          event.classList.add("active");
-        }
-      });
-    });
-
+    
   }
-
-
-
 }
